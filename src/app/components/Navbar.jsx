54f1c4899c2a954e81d002d29d89e97340e0d1d0
placeparks <86215469+placeparks@users.noreply.github.com>
2024-06-03 13:40:54 +0500

    Update Navbar.jsx

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
     <div>
       <nav className="relative bg-transparent dark:bg-gray-900 fixed w-full z-20 top-0 start-0 dark:border-gray-600">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <Link href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
+          <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
             <img src="/Group.png" className=" h-8 w-auto md:h-[72.42px] md:w-[123.81px]" alt="Logo" />
           </Link>
           <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
